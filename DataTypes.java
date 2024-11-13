f6311fdda0c3f19681eedae60422455bf660b403
ThushaN SilvA <ishanhansakasilva@gmail.com>
2024-11-13 15:28:11 +0530

    feat: add primitive data types

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //premitive data types
+
+        int num = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 6548845894l;
+
+        float f = 1234.56f;
+        double d = 132.45;
+
+        char c = 'q';
+
+        boolean t = true;
+    }
+}
