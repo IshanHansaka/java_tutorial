ae50069a666f8f77450de95c7d9f86bf624120ac
ishanhansaka <ishanhansakasilva@gmail.com>
2024-11-18 20:48:31 +0530

    feat: array

---
+++ Array.java
@@ -0,0 +1,19 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4};
+
+        for (int i = 0; i < arr.length;i ++) {
+            System.out.println(arr[i]);
+        }
+
+        int ary [] = new int[4];
+        //ary[0] = 5;
+        //ary[1] = 6;
+        //ary[2] = 7;
+        ary[3] = 8;
+
+        for (int i = 0; i < ary.length; i++) {
+            System.out.println(ary[i]);
+        }
+    }
+}
