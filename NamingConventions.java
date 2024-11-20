5afce72ec159c8e45c5c69cd74b3cd551f7a6002
ishanhansaka <ishanhansakasilva@gmail.com>
2024-11-20 23:08:20 +0530

    feat: naming conventions

---
+++ NamingConventions.java
@@ -0,0 +1,9 @@
+public class NamingConventions {
+    public static void main(String[] args) {
+        //class and interfaces - Human, Phone
+        //variables - man. car
+        //constans - PIE, BAND
+
+        //methods - getName() setAge()
+    }
+}
