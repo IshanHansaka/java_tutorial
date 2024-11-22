4b338f271e0e53e49e78597a10559b24c968a557
ishanhansaka <ishanhansakasilva@gmail.com>
2024-11-22 12:16:42 +0530

    feat: packages

---
+++ Packages.java
@@ -0,0 +1,12 @@
+import sample.Package01;
+import sample.Package02;
+
+public class Packages {
+    public static void main(String[] args) {
+    Package01 p1 = new Package01();
+    p1.show();
+    
+    Package02 p2= new Package02();
+    p2.show();
+    }
+}
