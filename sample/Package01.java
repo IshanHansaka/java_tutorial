41c77e788e81ab753463dbcfea7bfe1e765b79f7
ishanhansaka <ishanhansakasilva@gmail.com>
2024-11-22 12:14:48 +0530

    feat: create sample package

---
+++ sample/Package01.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class Package01 {
+    public void show() {
+        System.out.println("In package01");
+    }
+}
---
+++ sample/Package02.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class Package02 {
+    public void show() {
+        System.out.println("In package02");
+    }
+}
