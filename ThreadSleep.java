84038debc5aabe6469c92f778d0993f2d3045f10
ishanhansaka <ishanhansakasilva@gmail.com>
2024-11-23 17:35:30 +0530

    feat: thread sleep

---
+++ ThreadSleep.java
@@ -0,0 +1,40 @@
+public class ThreadSleep {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.start();
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        obj2.start();
+    }
+}
+
+class A extends Thread{
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class B extends Thread {
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
