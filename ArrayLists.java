31fc5d9f70ad15f6ac13867a84b498c335326c45
ishanhansaka <ishanhansakasilva@gmail.com>
2024-11-24 18:41:57 +0530

    feat: array list

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,18 +1,15 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class ArrayLists {
     public static void main(String[] args){   
-    	
-//    	Collection<Integer> nums= new ArrayList<Integer>();
-//  	Collection nums=new ArrayList();
+
     	List<Integer> nums=new ArrayList<Integer>();
+
     	nums.add(6);
     	nums.add(5);
     	nums.add(8);
     	nums.add(2);
-    	//nums.add("5");
     	
     	System.out.println(nums.get(2));
     	
